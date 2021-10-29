9a07194154b49ab750e932da6562b67aa2b1ebbc
Vanessa-Mayumi <82035304+Vanessa-Mayumi@users.noreply.github.com>
2021-10-29 00:22:02 -0300

    s6
    commit 6

---
+++ src/routes/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Switch, Route} from 'react-router-dom';
+import { Register } from '../pages/Register';
+import { Login } from '../pages/Login';
+import { Dashboard } from '../pages/Dashboard';
+import { NotFound } from '../pages/NotFound';
+
+
+const Routes: React.FC = () => (
+    <Switch>
+        <Route path="/" exact component={Login} />
+        <Route path="/register"  exact component={Register} />
+        <Route path="/dashboard" exact component={Dashboard} />
+        <Route path="/*" exact component={NotFound} />
+    </Switch>
+)
+
+export default Routes;
